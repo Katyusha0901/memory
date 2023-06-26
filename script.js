b58246f9dc5c0223b9d6f7281890a363698b9b42
Ekaterina Bondareva <1978231@bk.ru>
2023-06-26 16:47:45 +0700

    correct

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const field = [
   ["*", "*", "*", "*"],
   ["*", "*", "*", "*"],
 ];
-const givenField = createGivenField();
+const fieldWithKnownValues = createfieldWithKnownValues();
 startParty();
 
 function startParty() {
@@ -15,25 +15,25 @@ function startParty() {
 }
 
 //==========================================================================================================================================================
-//Creating givenField
+//Creating fieldWithKnownValues
 //==================================================================================================================================================================
 
 function shuffle(array) {
   array.sort(() => Math.random() - 0.5);
 }
 
-function createGivenField() {
-  let givenField = [
+function createfieldWithKnownValues() {
+  let fieldWithKnownValues = [
     [1, 2, 3, 4],
     [5, 6, 7, 8],
     [1, 2, 3, 4],
     [5, 6, 7, 8],
   ];
-  givenField.forEach((array) => {
+  fieldWithKnownValues.forEach((array) => {
     shuffle(array);
   });
-  shuffle(givenField);
-  return givenField;
+  shuffle(fieldWithKnownValues);
+  return fieldWithKnownValues;
 }
 
 //==========================================================================================================================================================
@@ -45,16 +45,16 @@ function requestCoordinates() {
     `Поле выглядит так\n${field[0]}\n${field[1]}\n${field[2]}\n${field[3]}\n Укажите 2 места на поле. Введите значения координат строкой 'xyxy' где х - номер строки, a y - номер столбца`
   );
 
-  const arr = coordinates
+  const array = coordinates
     .split("")
     .every((e) => ["0", "1", "2", "3"].includes(e));
-  if (arr === false) {
-    requestCoordinates(field);
-  } else {
+  if (array) {
     return [
       { y: coordinates[0], x: coordinates[1] },
       { y: coordinates[2], x: coordinates[3] },
     ];
+  } else {
+    return requestCoordinates();
   }
 }
 
@@ -63,29 +63,28 @@ function requestCoordinates() {
 //==================================================================================================================================================================
 
 function showSelectedFigures(coordinates) {
-  const immuField = field.map((array) => {
+  const clonedField = field.map((array) => {
     return [...array];
   });
-  for (let i = 0; i < 2; i++) {
-    immuField[coordinates[i].y][coordinates[i].x] =
-      givenField[coordinates[i].y][coordinates[i].x];
-  }
+  clonedField[coordinates[0].y][coordinates[0].x] =
+    fieldWithKnownValues[coordinates[0].y][coordinates[0].x];
+  clonedField[coordinates[1].y][coordinates[1].x] =
+    fieldWithKnownValues[coordinates[1].y][coordinates[1].x];
+
   alert(
-    `Попробуйте еще раз \n${immuField[0]}\n${immuField[1]}\n${immuField[2]}\n${immuField[3]}`
+    `Попробуйте еще раз \n${clonedField[0]}\n${clonedField[1]}\n${clonedField[2]}\n${clonedField[3]}`
   );
 }
 
 function checkEqualitySetSelected(coordinates) {
-  let arrForFigures = [];
-  for (let i = 0; i < 2; i++) {
-    arrForFigures.push(givenField[coordinates[i].y][coordinates[i].x]);
-  }
-  if (arrForFigures[0] === arrForFigures[1]) {
+  if (
+    fieldWithKnownValues[coordinates[0].y][coordinates[0].x] ===
+    fieldWithKnownValues[coordinates[1].y][coordinates[1].x]
+  ) {
     for (let i = 0; i < 2; i++) {
       field[coordinates[i].y][coordinates[i].x] =
-        givenField[coordinates[i].y][coordinates[i].x];
+        fieldWithKnownValues[coordinates[i].y][coordinates[i].x];
     }
-    startParty();
   }
   startParty();
 }
